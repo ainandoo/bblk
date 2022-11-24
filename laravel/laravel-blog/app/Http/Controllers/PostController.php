c36321332229a12951b08704e5d1232c7d1b8616
Ainando Kei <ainandoo@gmail.com>
2022-11-24 12:45:16 +0700

    update: laravel blog: read, edit, delete

--- laravel/laravel-blog/app/Http/Controllers/PostController.php
+++ laravel/laravel-blog/app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -40,5 +40,48 @@ class PostController extends Controller
         ]);
 
         return redirect() -> route('posts.index') -> with(['success' => 'Data berhasil disimpan!']);
+    }
+
+    public function show($id) {
+
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
+    }
+
+    public function edit(Post $post){
+        return view ('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post){
+        $this->validate($request, [
+            'gambar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'judul' => 'required|min:5',
+            'konten' => 'required|min:10'
+        ]);
+
+        if ($request -> hasFile('gambar')) {
+            $image = $request -> file('gambar');
+            $image -> storeAs('public/posts', $image -> hashName());
+
+            Storage::delete('public/posts' . $post -> gambar);
+
+            $post -> update([
+                'gambar' => $image -> hashName(),
+                'judul' => $request -> judul,
+                'konten' => $request -> konten
+            ]);
+        } else {
+            $post -> update([
+                'judul' => $request -> judul,
+                'konten' => $request -> konten
+            ]);
+        }
+        return redirect() -> route('posts.index') -> with(['success' => 'Data berhasil diubah!']); 
+    }
+
+    public function destroy(Post $post){
+        Storage::delete('public/posts' . $post -> gambar);
+        $post -> delete();
+        return redirect() -> route('posts.index') -> with(['success' => 'Data berhasil dihapus!']);
     }
 }
